f4bb5ec9ade39b857de298d9186096cd8a5f5267
spookyghost <spookyghost@mailbox.org>
2021-08-09 17:32:01 +0200

    Add Orbs

--- project/src/main/kotlin/cga/exercise/game/Scene.kt
+++ project/src/main/kotlin/cga/exercise/game/Scene.kt
@@ -3,7 +3,6 @@ package cga.exercise.game
 import cga.exercise.components.camera.TronCamera
 import cga.exercise.components.geometry.GeometryBuffer
 import cga.exercise.components.light.Light
-import cga.exercise.components.light.PointLight
 import cga.exercise.components.shader.ShaderProgram
 import cga.exercise.components.sound.SoundContext
 import cga.exercise.components.sound.SoundListener
@@ -34,34 +33,21 @@ class Scene(private val window: GameWindow) {
     private val quad = Quad()
     // private val level: Level
     private val gameObjects: MutableList<GameObject> = mutableListOf()
-    private val gBufferShader: ShaderProgram
+    private val gBufferShader = GBufferShader()
     private val gBuffer = GeometryBuffer(window)
-    private val deferredShader: ShaderProgram
-    private val skybox = Skybox.invoke("assets/textures/skybox")
-    private val skyboxShader: ShaderProgram
+    private val deferredShader = DefferedShader()
 
     //scene setup
     init {
+
         //initial opengl state
         glClearColor(0f, 0f, 0f, 1.0f); GLError.checkThrow()
         glEnable(GL_CULL_FACE); GLError.checkThrow()
         glFrontFace(GL_CCW); GLError.checkThrow()
         glCullFace(GL_BACK); GLError.checkThrow()
-        glEnable(GL_DEPTH_TEST); GLError.checkThrow()
+        glDisable(GL_DEPTH_TEST); GLError.checkThrow()
         glDepthFunc(GL_LESS); GLError.checkThrow()
-        deferredShader = ShaderProgram(
-            "assets/shaders/components/shader/deferredVert.glsl",
-            "assets/shaders/components/shader/deferredFrag.glsl",
-        )
-        gBufferShader = ShaderProgram(
-            vertexShaderPath = "assets/shaders/components/shader/gVert.glsl",
-            geometryShaderPath = "assets/shaders/components/shader/gGeom.glsl",
-            fragmentShaderPath = "assets/shaders/components/shader/gFrag.glsl",
-        )
-        skyboxShader = ShaderProgram(
-            vertexShaderPath = "assets/shaders/components/shader/skyboxVert.glsl",
-            fragmentShaderPath = "assets/shaders/components/shader/skyboxFrag.glsl"
-        )
+
         gameObjects.addAll(
             listOf(
                 Player(),
@@ -74,79 +60,56 @@ class Scene(private val window: GameWindow) {
         camera = TronCamera()
         camera.rotateLocal(-0.65f, 0.0f, 0f)
         camera.translateLocal(Vector3f(0f, 0f, 4f))
-        camera.parent = (gameObjects.first() as Player).model.renderable
 
         //Sound and level
         SoundContext.setup()
         // level = Level("caramelldansen")
         // level.setup()        GLError.checkThrow("Failed init")
-        skybox.setup()
-        skyboxShader.use()
-        skyboxShader.setUniform("skybox",0)
-        //Setup uniforms for deferredShader (Texture locations = output of gbuffer)
+
+        //Setup Uniforms
         deferredShader.use()
+
+
+    }
+
+    fun render(dt: Float, t: Float) {
+        // geometry pass into gbuffer
+        deferredRender(dt, t)
+        SoundListener.setPosition(camera)
+        GLError.checkThrow()
+    }
+
+
+    private fun deferredRender(dt: Float, t: Float){
+        gBuffer.bind()
+        glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
+        gBufferShader.use()
+        gBufferShader.setUniform("beat",t)
+        gameObjects.forEach{it.draw(gBufferShader)}
+        camera.bind(gBufferShader)
+        glBindFramebuffer(GL_FRAMEBUFFER, 0) //return to default
+
+        //Lighting Pass
+        glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
+        initLightRendering()
+        deferredShader.use()
+        gBuffer.bindTextures()
+        GLError.checkThrow()
         deferredShader.setUniform("inPosition", 0)
         deferredShader.setUniform("inNormal", 1)
         deferredShader.setUniform("inDiffuse", 2)
         deferredShader.setUniform("inSpecular", 3)
         deferredShader.setUniform("inEmissive", 4)
         deferredShader.setUniform("shininess", 64f)
-
-    }
-
-    fun render(dt: Float, t: Float) {
-        deferredRender(dt, t)
-        renderSkybox()
-        SoundListener.setPosition(camera)
-        GLError.checkThrow()
-    }
-
-    private fun renderSkybox() {
-        glDepthFunc(GL_LEQUAL)
-        skyboxShader.use()
-        //We eliminate the translation part of the matrix
-        val viewMatrix = Matrix4f(Matrix3f(camera.getCalculateViewMatrix()))
-        skyboxShader.setUniform("view_matrix", viewMatrix)
-        skyboxShader.setUniform("projection_matrix", camera.getCalculateProjectionMatrix())
-        skybox.render()
-        glDepthFunc(GL_LESS)
-    }
-
-
-    private fun deferredRender(dt: Float, t: Float){
-        //Geometry pass into gBuffer
-        gBuffer.bind()
-        glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
-        gBufferShader.use()
-        gBufferShader.setUniform("beat",t)
-        camera.bind(gBufferShader)
-        gameObjects.forEach{it.draw(gBufferShader)}
-        glBindFramebuffer(GL_FRAMEBUFFER, 0) //return to default
-
-        //Lighting Pass
-        //We can clear here, because we bound the default FBO again
-        glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
-        initLightRendering()
-        deferredShader.use()
-        gBuffer.bindTextures()
-        GLError.checkThrow()
-
         gameObjects.forEach { it.processLighting(deferredShader, camera.viewMatrix) }
         Light.bindAmount(deferredShader)
         sceneLights.forEach { it.bind(deferredShader, camera.viewMatrix) }
         //level.update(dt, t)
-        endLightRendering()
         quad.draw(deferredShader)
+        endLightRendering()
 
-        glBindFramebuffer(GL_READ_FRAMEBUFFER, gBuffer.gBufferID)
-        glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0)//write to default
-        //copy the depth buffer from gbuffer to default
-        glBlitFramebuffer(
-            0,0, window.windowWidth, window.windowHeight,
-            0,0, window.windowWidth, window.windowHeight,
-            GL_DEPTH_BUFFER_BIT, GL_NEAREST
-        )
-        glBindFramebuffer(GL_FRAMEBUFFER, 0) //set both to default again
+        glBindFramebuffer(GL_FRAMEBUFFER, 0) //return to default
+
     }
 
     private fun initLightRendering(){
@@ -201,38 +164,22 @@ class Scene(private val window: GameWindow) {
             x = xPos
             // Bike parent von Camera
             // bike <- rotateAround <- local <- v
-            camera.rotateAroundPoint(0f, diff.toFloat(), 0f, gameObjects.first().getPosition())
+            camera.rotateAroundPoint(0f, diff.toFloat(), 0f, Vector3f(0f))
         }
-        //  if (y == 0.0) {
-        //      y = yPos
-        //  } else {
-        //      val diff = (y - yPos) * 0.002
-        //      y = yPos
-        //      // Bike parent von Camera
-        //      // bike <- rotateAround <- local <- v
-        //      camera.rotateAroundPoint(diff.toFloat(), 0f, 0f, gameObjects.first().getPosition())
-        //  }
+        if (y == 0.0) {
+            y = yPos
+        } else {
+            val diff = (y - yPos) * 0.002
+            y = yPos
+            // Bike parent von Camera
+            // bike <- rotateAround <- local <- v
+            camera.rotateAroundPoint(diff.toFloat(), 0f, 0f, Vector3f(0f))
+        }
     }
 
     fun cleanup() {
         //level.cleanup()
         SoundContext.cleanup()
-    }
-
-    fun sceneLights(): MutableList<PointLight> {
-        val l = arrayListOf<PointLight>()
-        val r = Random(10)
-        val rf = { r.nextFloat() * 20f - 10f }
-        val ra = { r.nextFloat() * 0.5f }
-        for (i in 1 until NR_POINT_LIGHTS) {
-            val pl = PointLight(
-                    Vector3f(r.nextFloat(), r.nextFloat(), r.nextFloat()),
-                    Vector3f(1f, 0.15f, 0.12f),
-                )
-            pl.translateLocal(Vector3f(rf(), ra(), rf()))
-            l.add(pl)
-        }
-        return l // .toList()
     }
 }
 
---
+++ project/src/main/kotlin/cga/exercise/game/gameObjects/orb/Orb.kt
@@ -0,0 +1,61 @@
+package cga.exercise.game.gameObjects.orb
+
+import cga.exercise.components.geometry.Transformable
+import cga.exercise.components.light.PointLight
+import cga.exercise.components.shader.ShaderProgram
+import cga.exercise.game.gameObjects.GameObject
+import cga.framework.GameWindow
+import cga.framework.Random
+import org.joml.Matrix4f
+import org.joml.Vector3f
+import kotlin.math.PI
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.math.pow
+
+class Orb:Transformable(), GameObject {
+    private val color = Random.nextColor()
+    val model: OrbModel = OrbModel(color)
+    val light = PointLight(color, Vector3f(1f, 0.15f, 0.12f))
+    val direction = Random.nextVec3(-2f*PI.toFloat(), 2f* PI.toFloat())
+
+    init {
+        model.renderable.scaleLocal(Vector3f(0.2f))
+        model.renderable.parent = this
+        light.parent = this
+        translateLocal(Vector3f(0f,Random.nextFloat(1f,10f),0f))
+    }
+
+    override fun draw(shaderProgram: ShaderProgram) {
+        model.draw(shaderProgram)
+    }
+
+    override fun update(dt: Float, t: Float) {
+        val beat = t
+        val speed = 16f * (beat%1 - 0.5f).pow(4) + 0.2f
+        rotateAroundPoint(
+            direction.x * dt * speed,
+            direction.y * dt * speed,
+            direction.z * dt * speed,
+            Vector3f(0f)
+        )
+        direction.x = dir(direction.x)
+        direction.y = dir(direction.y)
+        direction.z = dir(direction.z)
+        model.update(dt, beat)
+    }
+
+    private fun dir(float: Float): Float {
+        return max(
+            min(float + Random.nextFloat(-0.1f,0.1f), 2f* PI.toFloat()),
+            -2f* PI.toFloat()
+        )
+    }
+
+    override fun processInput(window: GameWindow, dt: Float) {
+    }
+
+    override fun processLighting(shaderProgram: ShaderProgram, viewMatrix4f: Matrix4f) {
+        light.bind(shaderProgram,viewMatrix4f)
+    }
+}
